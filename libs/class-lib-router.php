ae64f8a388232784ec10bf9baa00f434662c7e56
Ligma Balls <ligma@balls.com>
2021-11-11 09:09:29 +0200

    patches for website deployment since local dev environment is different

--- libs/class-lib-router.php
+++ libs/class-lib-router.php
@@ -30,9 +30,7 @@ class Router {
 
         $request_url = filter_var( $_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL );
 
-        // $request_url = rtrim( $request_url, ( basename( ABSPATH ) . '/' ) );
-
-        $request_url = trim( $request_url, '/' );
+        $request_url = rtrim( $request_url, '/' );
 
         $request_url = strtok( $request_url, '?' );
 
